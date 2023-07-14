25e794327f3dfaf4988da190ec4c0a65293b7ba2
Heaven31415 <heaven31415@gmail.com>
2023-07-14 15:18:49 +0200

    Add Vehicle entity (without relations)

---
+++ migrations/Version20230714130652.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230714130652 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add vehicles table without relations';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            'CREATE TABLE vehicles (id INT AUTO_INCREMENT NOT NULL, swapi_id INT NOT NULL, name VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, vehicle_class VARCHAR(255) NOT NULL, manufacturer VARCHAR(255) NOT NULL, length VARCHAR(255) NOT NULL, cost_in_credits VARCHAR(255) NOT NULL, crew VARCHAR(255) NOT NULL, passengers VARCHAR(255) NOT NULL, max_atmosphering_speed VARCHAR(255) NOT NULL, cargo_capacity VARCHAR(255) NOT NULL, consumables VARCHAR(255) NOT NULL, created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', edited DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE vehicles');
+    }
+}
---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+#[ORM\Table(name: 'vehicles')]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $swapiId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $vehicleClass = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $manufacturer = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $length = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $costInCredits = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $crew = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $passengers = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $maxAtmospheringSpeed = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cargoCapacity = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $consumables = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $created = null;
+
+    #[ORM\Column]
+    private ?DateTimeImmutable $edited = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSwapiId(): ?int
+    {
+        return $this->swapiId;
+    }
+
+    public function setSwapiId(int $swapiId): static
+    {
+        $this->swapiId = $swapiId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getVehicleClass(): ?string
+    {
+        return $this->vehicleClass;
+    }
+
+    public function setVehicleClass(string $vehicleClass): static
+    {
+        $this->vehicleClass = $vehicleClass;
+
+        return $this;
+    }
+
+    public function getManufacturer(): ?string
+    {
+        return $this->manufacturer;
+    }
+
+    public function setManufacturer(string $manufacturer): static
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+
+    public function getLength(): ?string
+    {
+        return $this->length;
+    }
+
+    public function setLength(string $length): static
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getCostInCredits(): ?string
+    {
+        return $this->costInCredits;
+    }
+
+    public function setCostInCredits(string $costInCredits): static
+    {
+        $this->costInCredits = $costInCredits;
+
+        return $this;
+    }
+
+    public function getCrew(): ?string
+    {
+        return $this->crew;
+    }
+
+    public function setCrew(string $crew): static
+    {
+        $this->crew = $crew;
+
+        return $this;
+    }
+
+    public function getPassengers(): ?string
+    {
+        return $this->passengers;
+    }
+
+    public function setPassengers(string $passengers): static
+    {
+        $this->passengers = $passengers;
+
+        return $this;
+    }
+
+    public function getMaxAtmospheringSpeed(): ?string
+    {
+        return $this->maxAtmospheringSpeed;
+    }
+
+    public function setMaxAtmospheringSpeed(string $maxAtmospheringSpeed): static
+    {
+        $this->maxAtmospheringSpeed = $maxAtmospheringSpeed;
+
+        return $this;
+    }
+
+    public function getCargoCapacity(): ?string
+    {
+        return $this->cargoCapacity;
+    }
+
+    public function setCargoCapacity(string $cargoCapacity): static
+    {
+        $this->cargoCapacity = $cargoCapacity;
+
+        return $this;
+    }
+
+    public function getConsumables(): ?string
+    {
+        return $this->consumables;
+    }
+
+    public function setConsumables(string $consumables): static
+    {
+        $this->consumables = $consumables;
+
+        return $this;
+    }
+
+    public function getCreated(): ?DateTimeImmutable
+    {
+        return $this->created;
+    }
+
+    public function setCreated(DateTimeImmutable $created): static
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getEdited(): ?DateTimeImmutable
+    {
+        return $this->edited;
+    }
+
+    public function setEdited(DateTimeImmutable $edited): static
+    {
+        $this->edited = $edited;
+
+        return $this;
+    }
+}
